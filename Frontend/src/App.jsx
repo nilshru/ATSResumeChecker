be400beff4b7e28a7d9a659924437b989f8d0827
nilshru <nk9955688176@gmail.com>
2025-08-03 21:42:08 +0530

    setup project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen bg-gray-100">
+        {/* Main Content */}
+        <div className="flex-grow flex items-center justify-center flex-col">
+          <h1 className="text-3xl font-bold">ATS Resume Checker</h1>
+          <p>Under Construction</p>
+        </div>
+
+        {/* Footer */}
+        <footer className="bg-gray-700 text-center p-4">
+          <p className="text-white">Amity University &copy; 2025 ATS Resume Checker. All rights reserved.</p>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
