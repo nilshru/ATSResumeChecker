9db022db5a53ee38df054a0222b65b3cce8113e2
nilshru <nk9955688176@gmail.com>
2025-08-04 13:10:33 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,12 +7,12 @@ function App() {
         {/* Main Content */}
         <div className="flex-grow flex items-center justify-center flex-col">
           <h1 className="text-3xl font-bold">ATS Resume Checker</h1>
-          <p>Under Construction</p>
+          <p>Under Construction Stay Tuned</p>
         </div>
 
         {/* Footer */}
         <footer className="bg-gray-700 text-center p-4">
-          <p className="text-white">Amity University, Patna &copy; 2025 ATS Resume Checker. All rights reserved.</p>
+          <p className="text-white">&copy; Amity University, Patna  2025 ATS Resume Checker. All rights reserved.</p>
         </footer>
       </div>
     </>
